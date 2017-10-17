05d0d8de3aaf9c91b350ce45e002846c98ef7348
mpacek <m.pacek@onet.eu>
2017-10-17 15:43:47 +0200

    Image loaders

--- webpack.config.js
+++ webpack.config.js
@@ -5,20 +5,33 @@ const config = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    publicPath: 'dist/' // url loader, (and other loaders) adds this before assests name to have correct url
   },
   module: {
     rules: [
       {
-        use: 'babel-loader',
-        test: /\.js$/ // use babel only on .js files
+        test: /\.js$/, // use babel only on .js files
+        use: 'babel-loader'
       },
       {
+        test: /\.css$/,
+        // we can apply css by js:
         // use: ['style-loader', 'css-loader'], // loaders are applied from right (first) to left (last)
+        // or we can use plugin to take all imported css and merge them into one file:
         loader: ExtractTextPlugin.extract({
           loader: 'css-loader'
-        }),
-        test: /\.css$/
+        })
+      },
+      {
+        test: /\.(jpe?g|png|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: { limit: 40000 }
+          },
+          'image-webpack-loader'
+        ]
       }
     ]
   },
