84196aa1d6871b1302707a5a2c6bbafe189d445f
mpacek <m.pacek@onet.eu>
2017-10-18 12:09:42 +0200

    Project added plugins for scripts

--- Project/webpack.config.js
+++ Project/webpack.config.js
@@ -1,11 +1,27 @@
 var webpack = require('webpack');
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const VENDOR_LIBS = [
+  'react',
+  'lodash',
+  'redux',
+  'react-redux',
+  'react-dom',
+  'faker',
+  'react-input-range',
+  'redux-form',
+  'redux-thunk'
+];
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    bundle: './src/index.js',
+    vendor: VENDOR_LIBS
+  },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: '[name].js' // It will produce bundle.js, and vendor.js files
   },
   module: {
     rules: [
@@ -19,5 +35,16 @@ module.exports = {
         test: /\.css$/
       }
     ]
-  }
+  },
+  plugins: [
+    // resolve the issue with the same libraries in bundle.js and vendor.js,
+    // it removes libraries which are present in vendor.js from bundle.js
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
+    // this plugin will add automaticly correct <script> tags into the html document
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ]
 };
