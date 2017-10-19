c840b24ae9bf1464e991b1bf07270dbc3eefb500
mpacek <m.pacek@onet.eu>
2017-10-19 09:43:14 +0200

    Project added comment in server

--- Project/server.js
+++ Project/server.js
@@ -5,6 +5,7 @@ const webpackConfig = require('./webpack.config.js');
 
 const app = express();
 
+// This will add configuration to serve project files, reload it on change etc.
 app.use(webpackMiddleware(webpack(webpackConfig)));
 
 app.listen(3050, () => console.log('Listening'));
